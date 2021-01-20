6e076486567c90b7db68621b52d5934c550d38af
India Kalff <indiakalff@gmail.com>
2021-01-20 11:35:17 +0000

    Added levels of difficulty

--- grid.js
+++ grid.js
@@ -3,6 +3,7 @@ let width
 let height
 let score
 let lives
+let level = 1
 let gameInProgress
 let cells
 let alienCells = []
@@ -25,18 +26,39 @@ const displayIntro = document.querySelector('#displayintro')
 const displayGame = document.querySelector('#displaygame')
 const displayLives = document.querySelector('#displaylives')
 const displayScore = document.querySelector('#displayscore')
+const displayLevel = document.querySelector('#displaylevel')
 const displayGameOver = document.querySelector('#displaygameover')
 const displayMessage = document.querySelector('#displaymessage')
 const playGame = document.querySelector('#playgame')
 const playAgain = document.querySelector('#playagain')
 const quitGame = document.querySelector('#quitgame')
+const levels = [
+  {
+    level: 1,
+    lives: 10,
+    bossAliens: 3,
+    offset: 4
+  },
+  {
+    level: 2,
+    lives: 20,
+    bossAliens: 9,
+    offset: 3
+  },
+  {
+    level: 3,
+    lives: 30,
+    bossAliens: 18,
+    offset: 2
+  }
+]
 
 
-
-startGame()
+//startGame()
 
 
 function startGame() {
+
   // Start game
   gameInProgress = true
 
@@ -45,8 +67,10 @@ function startGame() {
   height = 15
 
   // Set other variables
-  score = 0
-  lives = 33
+  if (level === 1) {
+    score = 0
+  }
+  lives = levels[level - 1].lives
 
   cells = []
   alienCells = []
@@ -55,9 +79,9 @@ function startGame() {
 
   // Default alien starting positions
   alienRows = 4
-  maxHorizOffset = 3
-  leftHorizOffset = 3
-  rightHorizOffset = 3
+  maxHorizOffset = levels[level - 1].offset
+  leftHorizOffset = levels[level - 1].offset
+  rightHorizOffset = levels[level - 1].offset
   verticalOffset = 0
   direction = 'right'
 
@@ -65,6 +89,7 @@ function startGame() {
   grid.innerHTML = ''
   displayScore.innerHTML = score
   displayLives.innerHTML = lives
+  displayLevel.innerHTML = level
   displayIntro.classList.add('hide')
   displayGameOver.classList.add('hide')
   displayGame.classList.remove('hide')
@@ -144,14 +169,21 @@ function moveCharacter(direction) {
 // Initialise aliens
 function generateAliens() {
   let alienCounter = 1
+
   for (let i = 0; i < cells.length; i++) {
     cells[i].classList.remove('alien')
     if (i >= (width * (verticalOffset)) && i % width >= leftHorizOffset && i % width < width - rightHorizOffset && i < width * (alienRows + verticalOffset)) {
       cells[i].classList.add('alien')
-      alienCells.push({ 'id': [alienCounter], 'position': i, 'alive': true })
+      alienCells.push({ 'id': [alienCounter], 'position': i, 'lives': 1, 'maxLives': 1 })
       aliensLeftAlive = alienCells
       alienCounter++
     }
+  }
+  for (let i = 0; i < levels[level - 1].bossAliens; i++) {
+    let randomAlien = Math.floor(Math.random() * alienCells.length)
+    cells[alienCells[randomAlien].position].classList.add('bossalien')
+    alienCells[randomAlien].lives = 2
+    alienCells[randomAlien].maxLives = 2
   }
 }
 
@@ -181,15 +213,16 @@ function moveAliens() {
   }
   let alienCounter = 1
   for (let i = 0; i < cells.length; i++) {
-    cells[i].classList.remove('alien')
-    cells[i].classList.remove('bullet')
-    cells[i].classList.remove('bomb')
-    cells[i].classList.remove('hittarget')
+    cells[i].classList = ''
     cells[characterPosition].classList.add('character')
     if (i >= (width * (verticalOffset)) && i % width >= leftHorizOffset && i % width < width - rightHorizOffset && i < width * (alienRows + verticalOffset)) {
       alienCells[alienCounter - 1].position = i
-      if (alienCells[alienCounter - 1].alive) {
+      if (alienCells[alienCounter - 1].maxLives === 1 && alienCells[alienCounter - 1].lives === 1) {
         cells[i].classList.add('alien')
+      } else if (alienCells[alienCounter - 1].maxLives === 2 && alienCells[alienCounter - 1].lives === 2) {
+        cells[i].classList.add('bossalien')
+      } else if (alienCells[alienCounter - 1].maxLives === 2 && alienCells[alienCounter - 1].lives === 1) {
+        cells[i].classList.add('bossalienhalfdead')
       }
       alienCounter++
     }
@@ -208,14 +241,14 @@ function shoot() {
     while (i > 0) {
       document.querySelector(`#cell${i}`).classList.add('bullet')
       const aliensInBulletPath = alienCells.find(element => element.position === i)
-      if (Boolean(aliensInBulletPath) && aliensInBulletPath.alive === true) {
-        aliensInBulletPath.alive = false
+      if (Boolean(aliensInBulletPath) && aliensInBulletPath.lives > 0) {
+        aliensInBulletPath.lives--
         hitTarget(aliensInBulletPath.position)
         break
       }
       i -= width
     }
-    aliensLeftAlive = alienCells.filter(element => element.alive === true)
+    aliensLeftAlive = alienCells.filter(element => element.lives > 0)
   }
 }
 
@@ -243,8 +276,7 @@ function dropBomb() {
 function hitTarget(position) {
   score = score + 100
   displayScore.innerHTML = score
-  cells[position].classList.remove('alien')
-  cells[position].classList.remove('bullet')
+  cells[position].classList = ''
   cells[position].classList.add('hittarget')
   if (aliensLeftAlive.length === 1) {
     gameOver('win')
@@ -255,7 +287,9 @@ function hitTarget(position) {
 // Update lives when player character is bombed
 function takeDamage() {
   lives--
+  score = score - 50
   displayLives.innerHTML = lives
+  displayScore.innerHTML = score
   cells[characterPosition].classList.remove('bomb')
   cells[characterPosition].classList.remove('character')
   cells[characterPosition].classList.add('hittarget')
@@ -272,14 +306,25 @@ function gameOver(reason) {
   clearInterval(intervalPositionAliens)
   let message = ''
   let delay = 0
-  if (reason === 'win') {
-    message = `Congratulations - You win! Your final score was ${score}`
+  if (reason === 'win' && level < levels.length) {
+    message = `Congratulations - You survived level ${level} with a score of ${score}`
+    playAgain.innerHTML = `Next level: ${level + 1}`
+    level++
+    delay = 1000
+  } else if (reason === 'win' && level >= levels.length) {
+    message = `Congratulations - You have completed the game! Your final score was ${score}`
+    level = 1
+    playAgain.innerHTML = 'Play again'
     delay = 1000
   } else if (reason === 'lose') {
     message = `Sorry - you lost all your lives. Your final score was ${score}`
+    level = 1
+    playAgain.innerHTML = 'Play again'
     delay = 1000
   } else if (reason === 'quit') {
     message = 'You left the game'
+    level = 1
+    playAgain.innerHTML = 'Play again'
     delay = 0
   }
   setTimeout(() => {
